700a1927e1b51e5c4b31906448bb6813ed27bdb8
Lab Fox <labfoxdev@gmail.com>
2024-09-05 17:57:45 +0200

    gigtignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/labfox/Cours/venv"
+set -gx VIRTUAL_ENV "/home/administrateur/Git/Cours/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
